61d0149b3119982ec3f6a5170a6a84e41b693240
Burak Güney <burakguney@outlook.com.tr>
2021-06-28 15:25:22 +0300

    SSL

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const bodyParser = require('body-parser');
 const basicAuth = require('helpers/basicAuth');
 const errorHandler = require('helpers/errorHandler');
 
+const fs = require('fs'),
+    http = require('http'),
+    https = require('https');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
@@ -20,7 +24,13 @@ app.use('/', require('./api/apiController'));
 app.use(errorHandler);
 
 
-const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
-const server = app.listen(port, function () {
-    console.log('Server listening on port ' + port);
+const port = 8000;
+
+const options = {
+    key: fs.readFileSync('./SSL/key.pem'),
+    cert: fs.readFileSync('./SSL/cert.pem'),
+};
+
+const server = https.createServer(options, app).listen(port, function () {
+    console.log("Express server listening on port " + port);
 });
